462306426bb79aa101ef78a2368b58f20ea45e8d
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-03-25 09:24:51 -0700

    adds assertions to test correct movie added to "watched"

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -159,10 +159,11 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) == 1
     
-    raise Exception("Test needs to be completed.")
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    # raise Exception("Test needs to be completed.")
+    # Ensure the movie in "watched" is the correct one
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] == GENRE_1
+    assert updated_data["watched"][0]["rating"] == RATING_1
 
 # @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
