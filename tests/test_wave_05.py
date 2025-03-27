69f16367809511af3108587edb11dd924ce96e6f
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-03-26 19:28:53 -0700

    Added Act, Assert sections to wave 5 unit test: tests 1-3 passed

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -53,10 +53,13 @@ def test_new_genre_rec_from_empty_friends():
         ]
     }
 
-    raise Exception("Test needs to be completed.")
-    # *********************************************************************
-    # ****** Complete the Act and Assert Portions of these tests **********
-    # *********************************************************************
+    # Act
+    recommendations = get_new_rec_by_genre(sonyas_data)
+
+    # Assert
+    assert len(recommendations) == 0
+
+    
 
 # @pytest.mark.skip()
 def test_unique_rec_from_favorites():
